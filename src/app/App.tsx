9d2bad08f75c67c6cc16742ac2026a79e4d12e1c
LuiAkagiri <lui.music.work@gmail.com>
2026-08-30 14:12:36 +0900

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -886,7 +886,7 @@ function Contact() {
     setSubmitting(true);
     setError(null);
 
-    fetch("https://formspree.io/f/YOUR_FORM_ID", {
+    fetch("https://formspree.io/f/xdendggl", {
       method: "POST",
       body: data,
       headers: { Accept: "application/json" },
